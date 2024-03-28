f0553ba9b2390a71a6a017cae28f6a5cec0f8742
Alessandro-bttn <147080627+Alessandro-bttn@users.noreply.github.com>
2024-03-28 12:52:08 +0100

    Create Droni

---
+++ Droni.cpp
@@ -0,0 +1,12 @@
+//Coordinate XY
+//Stato InRicarica / InAzione
+//Ricarica massima 
+//Velocita
+//Batteria
+//Raggio d'azione
+//ID
+//costruttore / distruttore
+
+//[tempo][tipo mersaggio*][chi][a chi(opzionale)][cosa fa]
+//*ex mesaggio errore o di info
+
