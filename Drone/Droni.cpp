75a84ebec217ea8a9da142979a75ca60b2dffa9c
Alessandro-bttn <147080627+Alessandro-bttn@users.noreply.github.com>
2024-03-31 10:59:32 +0100

    Update Droni

--- Drone/Droni.cpp
+++ Drone/Droni.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 Droni::Droni(int X, int Y, char16_t stato, int batteria)
-{
-    AsseganazioneID();
+{   
+    ID = 0;
     MaxPwr = 100;
     Vel = 0; // i droni partono tutti dalla base ?!
     RoA = 6;
@@ -17,12 +17,3 @@ Droni::~Droni()
 {
 }
 
-void Droni::AsseganazioneID()
-{
-    //Prende il tempo corrente e lo trasforma e lo converte nell'id
-    auto now = std::chrono::system_clock::now();
-    auto now_seconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
-    auto epoch = now_seconds.time_since_epoch();
-    std::chrono::seconds::rep current_time_seconds = epoch.count();
-    ID = current_time_seconds;
-}
