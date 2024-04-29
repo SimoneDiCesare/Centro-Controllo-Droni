c48bd0c374043784692a187e398271b4a27ca0df
Alessandro-bttn <147080627+Alessandro-bttn@users.noreply.github.com>
2024-04-29 12:48:17 +0200

    Update monitor

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -5,17 +5,18 @@
 
 int main () {
     std::string fileName;
-    // Utilizza getline per ottenere una riga di input
     std::cout << "Inserire file .log: ";
     std::getline(std::cin, fileName); 
-
-    std::ifstream fileLog(fileName); // Apri direttamente il file specificato
+    std::cout << "\n";
+    std::ifstream fileLog(fileName);
 
     if (!fileLog.is_open()) {
         std::cerr << "Impossibile aprire il file " << fileName << std::endl;
         return 1; 
     }
 
+    bool error = false;
+    
     std::string line;
     std::vector<std::string> tokens;
     while (std::getline(fileLog, line)) {
@@ -25,21 +26,25 @@ int main () {
         size_t pos1 = line.find("[");
         size_t pos2 = line.find("]");
         while (pos1 != std::string::npos && pos2 != std::string::npos) {
-            // Estrai la sottostringa tra i delimitatori
             std::string token = line.substr(pos1 + 1, pos2 - pos1 - 1); 
             tokens.push_back(token);
-            // Trova la prossima occorrenza dei delimitatori nella riga
+
             pos1 = line.find("[", pos2 + 1);
             pos2 = line.find("]", pos1 + 1);
         }
-        
+
         for (const auto& t : tokens) {
             if (t == "ERROR"){
+                error = true;
                 std::cout << line << std::endl;
             }
         }
     }
 
+    if (error == false){
+        std::cerr << "Nel file "<< fileName << " non ci sono messaggi di errore." << std::endl;
+    }
+    
     fileLog.close();
     
     return 0;
