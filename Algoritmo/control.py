ee38c09bfd312c4db02c5f2023d293a6680423af
LavoroPulito <100956316+LavoroPulito@users.noreply.github.com>
2024-06-27 16:44:34 +0200

    progress

--- Algoritmo/control.py
+++ Algoritmo/control.py
@@ -32,6 +32,8 @@ def max_block(blocks):
             g = i
     return g
 
+def size(block): return ((block.down_right[0]-block.up_left[0])*(block.down_right[1]-block.up_left[1]))
+
 def create_blocks(start_dim, nblocks):
     #list of blocks:
     #[...
@@ -63,7 +65,42 @@ def biBlock(start_dim, nblock):
        if pow_of_two >= start_dim*start_dim:
            return create_blocks(start_dim, start_dim**2)
     return create_blocks(start_dim, pow_of_two)
-    
+
+def approxBlock(nDrones):
+    h= int(np.sqrt(nDrones))
+    w = h
+    while w * h < nDrones:
+        if (w+1)*h <= nDrones: w += 1
+        else:h += 1
+    print("h",h,"w",w)
+    return h, w
+
+def create_blocks_2(area_dim, nDrones):
+    blocks = []
+    if nDrones >= area_dim**2:
+        h = area_dim
+        w = area_dim
+    else: h,w = approxBlock(nDrones)
+    heightBlock = int((area_dim + (h -1))/h)
+    widthBlock = int((area_dim + (w -1))/w)
+    wasteHD = h*heightBlock - area_dim
+    wasteWR = w*widthBlock - area_dim
+    if wasteHD > heightBlock : 
+        wasteHU = wasteHD//2
+        wasteHD -= wasteHU
+    if wasteWR > heightBlock : 
+        wasteWL = wasteWR//2
+        wasteWR -= wasteWL
+    print("widthBlock: ",widthBlock,"wasteW: ", wasteWR)
+    for y in range(h-1):
+        for x in range(w-1):
+            blocks.append([heightBlock*y, widthBlock*x, heightBlock*(y+1), widthBlock*(x+1)])
+    for y in range(h-1):
+        blocks.append([heightBlock*y, widthBlock*(w-1), heightBlock*(y+1), widthBlock*(w)-wasteWR])
+    for x in range(w-1):
+        blocks.append([heightBlock*(h-1), widthBlock*(x), heightBlock*(h)-wasteHD, widthBlock*(x+1)])
+    blocks.append([heightBlock*(h-1), widthBlock*(w-1),heightBlock*(h)-wasteHD, widthBlock*(w)-wasteWR])
+    return blocks    
 
 def assegna(block, assignment, mtx):
     free = [b for b in block if b not in assignment.values()]
@@ -78,6 +115,7 @@ def assegna(block, assignment, mtx):
     
 def assegnaMax(block, assignment, mtx):
     free = [b for b in block if b not in assignment.values()]
+    if free == []: return None
     res = free[0]
     res_max = mtx[res.up_left[0]:res.down_right[0], res.up_left[1]:res.down_right[1]].max()
     for bl in free:
@@ -90,24 +128,6 @@ def assegnaMax(block, assignment, mtx):
 def calculate_time_diag(a,b):
     return int(np.sqrt(((a[0]-b[0])**2)+(a[1]-b[1])**2)+1)
 
-
-
-#-----------
-#DATI DI PARTENZA
-#ogni casella indica un'area quadrata di 14,142135624 m x 14,142135624 m 
-#velocita' del drone = 15 km/h
-#per passare da una casella all'altra un drone impiega 3.4 secondi. 
-#ogni STEP indica un'iterazione del codice
-#in questa iterazione il drone si muove di STEP
-#ogni STEP indica 3.4 secondi. 
-DIM_CASELLA = 14.142135624 
-LATO = 2000
-dim = int((LATO+DIM_CASELLA-0.000000001)/DIM_CASELLA) #per problema originale dim = 300
-common_time = 1061 #per problema originale dim = 1061
-Ndrones = 128
-origin = (int((dim-1)/2),int((dim-1)/2)) #presumendo che l'area sia QUADRATA
-t_iter = 20000
-#-----------
 #versione SENZA movimenti in diagonale: ogni spostamento è marcato sulla mappa
 def control(mat):
     drones = [dr.Drone(common_time, origin, d) for d in range(Ndrones)]
@@ -184,8 +204,9 @@ def control(mat):
     vis.salva(mat)
 #versione CON movimenti in diagonale: i movimenti in diagonale non modificano la mappa ne' la matrice.
 def controlD(mat):
+    print("dim: ", dim)
     drones = [dr.Drone(common_time, origin, d) for d in range(Ndrones)]
-    blocksIndex = biBlock(dim, Ndrones)
+    blocksIndex = create_blocks_2(dim, Ndrones//7)
     block = [blk.Block((b[0],b[1]),(b[2], b[3]),origin) for b in blocksIndex]
     recharge = [1061*4 for d in drones]
     dati = np.zeros((3,t_iter+1), dtype= float)
@@ -197,16 +218,17 @@ def controlD(mat):
     if block == []:
         return "troppi droni per la dimensione dell'area"
     for b in block:
-        print(b)
+        print(b,size(b))
     print(len(block))
     
-    for iteration in tqdm(range(t_iter), desc="peni"):
+    for iteration in range(t_iter):
         
         for r in [dro for dro in drones if dro.state == "Ready"]:
-            assignment[r] = assegnaMax( block, assignment,mat)
-            r.state = "Diagonal"
-            destination[r.id] = assignment[r].lastvisit
-            diagonal[r.id] = calculate_time_diag(r.position, destination[r.id])
+            assignment[r] = assegnaMax(block, assignment,mat)
+            if assignment[r] != None:
+                r.state = "Diagonal"
+                destination[r.id] = assignment[r].lastvisit
+                diagonal[r.id] = calculate_time_diag(r.position, destination[r.id])
 
             
         for d in drones:
@@ -220,56 +242,58 @@ def controlD(mat):
                 
                 
             else:
-                
-                if calculate_time_diag(d.position, origin) >= d.time_of_fly and d.state != "Diagonal": #se il drone è scarico rientra
-                    assignment[d] = None
-                    d.state = "Diagonal"
-                    destination[d.id] = origin
-                    diagonal[d.id] = d.time_of_fly
-                    #print(f"d.id: {d.id}, tof: {d.time_of_fly}, diag:{diagonal[d.id]}")
-
-                    
-                elif d.state == "Diagonal": 
-                    diagonal[d.id]-=1   
-                    if diagonal[d.id] == 0:
-                        d.position = destination[d.id]
-                        d.state = "Flying"
-                    
-                elif d.state =="Transferring":
-                    mat[d.position] = 0
-                    d.position = sd.go_to(d.position,assignment[d].lastvisit)
-                    
-
-                elif d.state == "Flying":
-                    mat[d.position] = 0
-                    new_pos = sd.scan(d.position, assignment[d], mat)
-
-                    if new_pos == (-1,-1):
-                        assignment[d].lastvisit = assignment[d].start
-                        assignment[d] = None
-                        assignment[d] = assegnaMax(block, assignment,mat)
-                        d.state = "Transferring"
-                        d.position = sd.go_to(d.position,assignment[d].lastvisit)
-                    else:
-                        d.position = new_pos
-                        assignment[d].lastvisit = d.position
-                
-                #Update state
                 if assignment[d] != None:
-                    if d.position == assignment[d].start or d.position == assignment[d].lastvisit: #se il drone è allo start "Starting"->"Flying"
-                        d.state = "Flying"
-                if(d.time_of_fly == 0):
-                    if (d.position == origin):
-                        d.state = "Charging"
-                    else:
-                        d.state = "Dead"
-                        print(f"Il drone {d.id} è morto nel punto:{d.position}")
-                d.time_of_fly-=1
-            #print(d, f"blocco: {assignment[d]}, diag: {diagonal[d.id]}" )
+                    
+                    if calculate_time_diag(d.position, origin) >= d.time_of_fly and d.state != "Diagonal": #se il drone è scarico rientra
+                        assignment[d] = None
+                        d.state = "Diagonal"
+                        destination[d.id] = origin
+                        diagonal[d.id] = d.time_of_fly
+                        #print(f"d.id: {d.id}, tof: {d.time_of_fly}, diag:{diagonal[d.id]}")
+
+                        
+                    elif d.state == "Diagonal": 
+                        diagonal[d.id]-=1   
+                        if diagonal[d.id] == 0:
+                            d.position = destination[d.id]
+                            d.state = "Flying"
+                        
+                    elif d.state =="Transferring":
+                        mat[d.position] = 0
+                        d.position = sd.go_to(d.position,assignment[d].lastvisit)
+                        
+
+                    elif d.state == "Flying":
+                        mat[d.position] = 0
+                        new_pos = sd.scan(d.position, assignment[d], mat)
+
+                        if new_pos == (-1,-1):
+                            assignment[d].lastvisit = assignment[d].start
+                            assignment[d] = None
+                            assignment[d] = assegnaMax(block, assignment,mat)
+                            d.state = "Transferring"
+                            d.position = sd.go_to(d.position,assignment[d].lastvisit)
+                        else:
+                            d.position = new_pos
+                            assignment[d].lastvisit = d.position
+                    
+                    #Update state
+                    if assignment[d] != None:
+                        if d.position == assignment[d].start or d.position == assignment[d].lastvisit: #se il drone è allo start "Starting"->"Flying"
+                            d.state = "Flying"
+                    if(d.time_of_fly == 0):
+                        if (d.position == origin):
+                            d.state = "Charging"
+                        else:
+                            d.state = "Dead"
+                            print(f"Il drone {d.id} è morto nel punto:{d.position}")
+                    d.time_of_fly-=1
+            print(d, f"blocco: {assignment[d]}, diag: {diagonal[d.id]}" )
         #print(recharge)
         #vis.visualizza(mat)
         mat = mat + 1
-        avg,maxx = mt.info(mat)
+        avg = np.mean(mat)
+        maxx = np.max(mat)
         in_volo = 0
         for d in drones:
             if d.state == "Starting" or d.state == "Flying":
@@ -284,14 +308,34 @@ def controlD(mat):
 
 
 if __name__ == "__main__":
+    #-----------
+    #DATI DI PARTENZA
+    #ogni casella indica un'area quadrata di 14,142135624 m x 14,142135624 m 
+    #velocita' del drone = 15 km/h
+    #per passare da una casella all'altra un drone impiega 3.4 secondi. 
+    #ogni STEP indica un'iterazione del codice
+    #in questa iterazione il drone si muove di STEP
+    #ogni STEP indica 3.4 secondi. 
+    DIM_CASELLA = 14.142135624 
+    LATO = 1000
+    dim = int((LATO+DIM_CASELLA-0.000000001)/DIM_CASELLA) #per problema originale dim = 300
+    common_time = 1061 #per problema originale dim = 1061
+    Ndrones = 14*14
+    origin = (int((dim-1)/2),int((dim-1)/2)) #presumendo che l'area sia QUADRATA
+    t_iter = 20000
+#-----------
     mat = np.full((dim, dim), 1)
     controlD(mat)
-            
-    """blks = create_bloks(dim, 16)
-    print(blks)
-    for y in range(dim):
-        for x in range(dim):
-            mat[y,x] = whitchBlock(blks, (y,x))"""
+    
+    # blks_idx = create_blocks_2(dim, 17)
+    # blks = [blk.Block((b[0],b[1]),(b[2], b[3]),origin) for b in blks_idx]
+    # print(blks)
+
+    # for y in range(dim):
+    #     for x in range(dim):
+    #         mat[y,x] = whitchBlock(blks, (y,x))
+    # print(mat)
+
     """b = [dr.Drone(10, (0,0)), dr.Drone(10, (0,1)), dr.Drone(10, (1,0))]
     a = [blk.Block((0,0),(1,1)), blk.Block((1,1),(2,2)), blk.Block((1,0),(2,1))]
     dic = dict(zip(b, a))
---
+++ ToDo.md
@@ -0,0 +1,21 @@
+# to do:
+## nella relazione
+- [ ] Aggiungere monitor 
+- [ ] Aggiungere risultati sperimentali 
+- [ ] database
+
+## Nel simulatore python
+- [ ] modificare divisioni blocchi python 
+- [ ] simulazioni test 
+
+## Nel resto del progetto
+- [ ] capire se i monitor sono giusti
+- [ ] capire come ottenere a run time informazioni dall'area monitorata
+    - [ ] e ottenerle
+- [ ] pulire la repository da file vecchi
+- [ ] Scrivere un bel [README](README.md)
+
+## domande di Armando 
+- [ ] perché ci sono due makefile? uno in [makefile1](libpqxx-7.7.5/build/include/Makefile) e [makefile2](makefile)
+
+- [ ] [Database.md](Database.md) è aggiornato? Se è aggiornato lo aggiungo alla relazione
